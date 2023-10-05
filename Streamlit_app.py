0114dbe0b3f908d41a5e8b66a0aa1327d81d8585
SWSINGH410 <146962222+SWSINGH410@users.noreply.github.com>
2023-10-05 20:56:12 +0300

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -29,7 +29,7 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-
+import snowflake.connector
 
 # json version of response nd normlize it.
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
